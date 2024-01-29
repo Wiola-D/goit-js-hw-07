9e874ef1f75179f258069cd1790eae62e4fdaeac
Wiola-D <wioletta.d.work@gmail.com>
2024-01-29 08:58:45 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,21 +5,15 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
-for (let item of galleryItems) {
-  const galleryItem = document.createElement("li");
+const galleryImages = galleryItems
+  .map(
+    (item) =>
+      `<li><a class = "gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></a></li>`
+  )
+  .join("");
 
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__item");
-  galleryLink.href = item.original;
+gallery.innerHTML = galleryImages;
 
-  const galleryImg = document.createElement("img");
-  galleryImg.classList.add("gallery__image");
-  galleryImg.src = item.preview;
-  galleryImg.alt = item.description;
-
-  galleryLink.append(galleryImg);
-  galleryItem.append(galleryLink);
-  gallery.append(galleryItem);
-}
-
-var lightbox = new SimpleLightbox(".gallery a", {});
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay,
+});
